06dab44d75ca5ab97384eb461813599e26ac1b08
Lorenzo <Loremente@outlook.it>
2022-02-05 22:18:35 +0100

    Logic Vanish

---
+++ src/main/java/me/loremente/vanish/VanishCommand.java
@@ -0,0 +1,40 @@
+package me.loremente.vanish;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+public class VanishCommand implements CommandExecutor {
+
+    private List<UUID> vanished = new ArrayList<>();
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+        if (sender instanceof Player) {
+            Player player = (Player) sender;
+
+            if (vanished.contains(player.getUniqueId())) {
+                vanished.remove(player.getUniqueId());
+                for (Player target : Bukkit.getOnlinePlayers()) {
+                    target.showPlayer(Player);
+                }
+                player.sendMessage(ChatColor.GREEN + "You unvanished!");
+            } else {
+                vanished.add(player.getUniqueId());
+                for (Player target : Bukkit.getOnlinePlayers()) {
+                    target.hidePlayer(player);
+                }
+                player.sendMessage("You vanished!");
+            }
+        }
+        return false;
+    }
+}
